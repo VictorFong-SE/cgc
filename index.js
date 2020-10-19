e549b71ad39918dbd6698efdf0daf613d6949079
Victor Fong <djtekno11@gmail.com>
2020-10-19 18:26:25 -0500

    swapped to commando framework, set up folders

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 //----------------------main--------------------------------//
 
-const { prefix, token } = require("./config.json");
-
-// require the discord.js module
-const Discord = require('discord.js');
-
-// create a new Discord client
-const client = new Discord.Client();
+// requires the discord.js module
+const {prefix, token, owner, invite} = require("./config.json");
+const {CommandoClient} = require('discord.js-commando');
+const path = require('path');
 
 
+const client = new CommandoClient({
+    commandPrefix: prefix,
+    owner: owner,
+    invite: invite,
+});
 
-// when the client is ready, run this code
-// this event will only trigger one time after logging in
+client.registry
+    .registerDefaultTypes()
+    .registerGroups([
+        ['fun', 'fun stuff'],
+        ['admin', 'Administrative'],
+        ['role', 'role based'],
+    ])
+    .registerDefaultGroups()
+    .registerDefaultCommands()
+    .registerCommandsIn(path.join(__dirname, 'commands'));
+
+
 client.once('ready', () =>
 {
-    console.log('Ready!');
-})
+    console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
+    client.user.setActivity('Being responsible');
+});
+
+client.on('error', console.error);
 
 
 // login to Discord with your app's token
 client.login(token);
+
